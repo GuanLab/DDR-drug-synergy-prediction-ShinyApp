b7dde50a115a1a17547c75384bc03620231a3ab3
Raye <rayezh@umich.edu>
2021-01-07 09:31:03 -0500

    initial commit

---
+++ DDR_synergy_prediction/app.R
@@ -0,0 +1,86 @@
+# example input: 
+# molecular biomarkers of treated cell lines
+
+
+
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+###### call lightGBM in R 
+require(lightgbm)
+
+###### call python in R package
+require(reticulate)
+require(DT)
+# load pre-trained lightGBM model
+use_python("/Users/rayezhang/anaconda3/bin/python")
+#py_install("pandas") #install pandas in R
+#py_install("lightgbm")
+#py_install("shap")
+source_python("./dependency/main_prediction.py")
+p_data <- read.table('./dependency/input/example_input.tsv', header = TRUE)
+#df_drugs <- predict_optimal_drug_combination(p_data)
+
+# Define UI for application that draws a histogram
+ui = basicPage(
+   # App title
+   titlePanel("Predict optimal drug combinations"),
+   
+   # Sidebar layout for input and output definitions
+   sidebarLayout(
+      # Sidebar panel for inputs
+      sidebarPanel(
+         # Input: select a file
+         fileInput("file1", "Upload patient's molecular biomarker readouts (tsv file)",
+                   multiple = TRUE,
+                   accept = c("text/table/tsv",
+                              "text/tab-separated-values,text/plain",
+                              ".tsv"))
+      ),
+    mainPanel(
+       DT::dataTableOutput("mytable")
+    )  
+      
+   )
+)
+   
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   output$mytable <- DT::renderDataTable({
+      
+      req = input$file1
+      p_data <- read.table(input$file1$datapath, header = TRUE)
+      df_drugs <- predict_optimal_drug_combination(p_data)
+      return(df_drugs)
+      }
+                                   )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+# put in quantile normalization 
+# select moa chemical 
+ #
+
+# grregate across different treatement 
+
+
+# a set of samples
+# cancer type/tissue/moa; summary graph ; box plots
+
+
+# take new cell lines
+
+# pick cell lines for specific moa combinations
+
+#surrogate model (important genes only)
+
+
---
+++ dependency/README.md
@@ -0,0 +1,14 @@
+this is code for shiny app to predict best drug combination treatements based on patient's genomic and transcriptomic readouts
+
+input format: 
+molecular readouts from patient
+
+output:
+predicted best drug combinations;
+predicted best drug efficacy
+predicted best drug synergy
+
+prerequisit:
+python 3
+lightGBM
+shap
