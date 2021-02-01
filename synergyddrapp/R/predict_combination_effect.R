247700e0b87b81fc195e1d16df5641197c6220e7
Julian Kreis <kreis.julian@gmx.de>
2021-02-01 15:01:24 +0100

    test different methods to retrieve shap values

--- synergyddrapp/R/predict_combination_effect.R
+++ synergyddrapp/R/predict_combination_effect.R
@@ -1,17 +1,19 @@
-# predict_combination_effect = function(X, feature_names, pred_target) {
+predict_combination_effect = function(X, feature_names, pred_target) {
     
-#     all_model_path = list.files(path = '../dependency/saved_models/', full.names=T,
-#                                 pattern = glue::glue("{pred_target}.*.model.txt$"))
-#     all_shap = c()
+    all_model_path = list.files(path = '../dependency/saved_models/', full.names=T,
+                                pattern = glue::glue("{pred_target}.*.model.txt$"))
+    all_shap = c()
+    browser()
+    preds = purrr::map(all_model_path, function(x) {
+        reg = lightgbm::lgb.load(x)
+        pred = reg$predict(X, predcontrib=TRUE)
+        
 
-#     preds = purrr::map(all_model_path, function(x) {
-#         reg = lightgbm::lgb.load(x)
-#         reg$predict(X)
-
-#         # TODO: shap score
-#         # lgb.trees <- lgb.model.dt.tree(reg) # First get a lgb tree
-#         # explainer <- lightgbmExplainer::buildExplainer(lgb.trees)
-#         # # compute contribution for each data point
-#         # pred.breakdown <- lightgbmExplainer::explainPredictions(reg, explainer, X)
-#     })
-# }
+        # SHAPforxgboost::shap.values(reg, X)
+        # TODO: shap score
+        lgb.trees <- lightgbm::lgb.model.dt.tree(reg) # First get a lgb tree
+        explainer <- lightgbmExplainer::buildExplainer(lgb.trees)
+        # compute contribution for each data point
+        pred.breakdown <- lightgbmExplainer::explainPredictions(reg, explainer, X)
+    })
+}
