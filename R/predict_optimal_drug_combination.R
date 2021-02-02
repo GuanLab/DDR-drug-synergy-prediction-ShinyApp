b5c867973d22a1fe206ad98d9434954e1a9103c0
ticarki <kreis.julian@gmx.de>
2021-02-02 12:05:40 +0100

    Update predict_optimal_drug_combination.R

--- R/predict_optimal_drug_combination.R
+++ R/predict_optimal_drug_combination.R
@@ -1,42 +1,43 @@
-
-
 #' Predict best drug combinations based on patient's molecular readouts
 #' 
 #' @param mol_df data.frame with molecualr readouts from patient
 #' @return best drug combinations based on prediction
 #' 
 #' @export
-predict_optimal_drug_combination = function(mol_df, data_path) {
+predict_optimal_drug_combination = function(mol_df) {
 
     
-    all_drugs = synergyddrapp::drug_summary
-
-    # create combination of all drugs and MoAs
-    .combn_pair = function(x, col) {
-        combn(x[[col]], m=2, simplify=F) %>% 
-            unlist %>% 
-            matrix(., ncol=2, byrow=T) %>%
-            data.frame()
-    }
-    tratment_comb_2 = .combn_pair(all_drugs, "drug_name") %>%
+    tratment_comb_2 <- .combn_pair(synergyddrapp::drug_summary,
+                                   "drug_name") %>%
         dplyr::rename(.metadata_treatment_1=X1, .metadata_treatment_2=X2)
-    moa_comb_2 = .combn_pair(all_drugs, "mode-of-action") %>%
+    
+    moa_comb_2 <- .combn_pair(synergyddrapp::drug_summary, "mode-of-action") %>%
         dplyr::rename(.metadata_moa_1=X1, .metadata_moa_2=X2)
     
-    df = cbind(moa_comb_2, tratment_comb_2)
+    df <- cbind(moa_comb_2, tratment_comb_2)
     
-    features = synergyddrapp::build_feature_dataset(data=df, mol=mol_df,  target="aoc", data_path=data_path)
+    # create a set of features
+    features <- build_feature_dataset(data=df, mol=mol_df, target="aoc")
 
     # predict efficacy:
-    pred_aoc = synergyddrapp::predict_combination_effect(X=features, pred_target='aoc')
-    feature_shap_aoc = synergyddrapp::predict_combination_effect(X=features, pred_target='aoc', predcontrib=TRUE)
+    pred_aoc         <- predict_combination_effect(X=features, pred_target='aoc')
+    feature_shap_aoc <- predict_combination_effect(X=features, pred_target='aoc',
+                                                  predcontrib=TRUE)
      
-    pred_bliss = synergyddrapp::predict_combination_effect(X=features, pred_target='bliss')
-    feature_shap_bliss = synergyddrapp::predict_combination_effect(X=features, pred_target='bliss', predcontrib=TRUE)
+    # create a set of features
+    features <- build_feature_dataset(data=df, mol=mol_df, target="bliss")
+    
+    pred_bliss         <- predict_combination_effect(X=features, 
+                                                    pred_target='bliss')
+    feature_shap_bliss <- predict_combination_effect(X=features, 
+                                                    pred_target='bliss', 
+                                                    predcontrib=TRUE)
 
-    prediction = df %>%
+    # append predictions to treatment/moa combinations
+    prediction <- df %>%
         dplyr::mutate(predicted_aoc = pred_aoc,
                       predicted_bliss = pred_bliss)
 
-    list(predictions=predictions, bliss_contributions=feature_shap_bliss, aoc_contribution=feature_shap_aoc)
+    list(predictions=prediction, bliss_contributions=feature_shap_bliss, 
+         aoc_contribution=feature_shap_aoc)
 }
