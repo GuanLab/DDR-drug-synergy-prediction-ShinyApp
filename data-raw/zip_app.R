e4edddf32ca0d87b42bd69486604226039b02968
Julian Kreis <kreis.julian@gmx.de>
2022-03-18 09:08:07 +0100

    Update zipped data

---
+++ data-raw/zip_app.R
@@ -0,0 +1,19 @@
+#' Save complete app requirements in compact format
+#'
+#' Zip all required data for the app. For the app usage no other data than the
+#' zip file is needed
+#'
+#' Author: julian.kreis@external.merkcgroup.com
+#' Date: Fri Mar  18 08:48:11 2022
+#' --------------
+
+my_wd      = getwd()
+dest_path  = here::here('inst/app/')
+setwd(dest_path)
+
+files = list.files(dest_path)
+
+zip(zipfile = here::here("inst/extdata/app_data.zip"), 
+    files = files)
+
+setwd(my_wd)
